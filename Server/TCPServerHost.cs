5cc3dcf7c44a5f892b8506937cc4456e930c971c
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-02-27 19:54:51 +1000

    Addition of the Host and Slave of the TCP server. Implementation is still needed

---
+++ Server/TCPServerHost.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SCPSLRCon.Server;
+using SCPSLRCon.Core.Config;
+using System.Net.Sockets;
+using System.Net;
+
+namespace SCPSLRCon.Server
+{
+    internal class TCPServerHost
+    {
+        public CoreConfig CoreConfig;
+
+        public Thread CreateServer()
+        {
+            Thread currentThread = new Thread(Server)
+            {
+                IsBackground = true
+            };
+            currentThread.Start(CoreConfig.TcpServerPort);
+
+            return currentThread;
+        }
+
+        private static void Server(object port)
+        {
+            int intPort;
+
+            try
+            {
+                intPort = (int)port;
+            }
+            catch
+            {
+                throw new Exception("The TCP Server Port is not an integer, please restart the server after fixing the TCP Server Port in SCP:SL RCon's \"CoreConfig.yml\" file.");
+            }
+
+            TcpListener listener = new TcpListener(IPAddress.Any, intPort);
+
+            while (true)
+            {
+                Socket currentSocket = listener.AcceptSocket();
+
+
+
+            }
+        }
+
+        /*
+         * THIS WILL NOT DESTROY CONNECTIONS!!! 
+         * This only destroys the receiver.
+        */
+        public void DestroyServer(Thread thread) {
+            thread.Abort();
+        }
+    }
+}
---
+++ Server/TCPServerSlave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCPSLRCon.Server
+{
+    internal static class TCPServerSlave
+    {
+        public static void Slave(object Socket)
+        {
+
+        }
+    }
+}
